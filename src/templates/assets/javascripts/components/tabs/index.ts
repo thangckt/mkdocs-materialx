9a0a9a396f3c6cecfce9d677448dc55e275989d3
C. Thang Nguyen <thangckt@gmail.com>
2026-05-25 00:59:53 +0900

    Update index.ts

--- src/templates/assets/javascripts/components/tabs/index.ts
+++ src/templates/assets/javascripts/components/tabs/index.ts
@@ -154,15 +154,15 @@ function setupTabsOverflow(el: HTMLElement): void {
 
   triggerBtn.addEventListener("click", ev => {
     ev.stopPropagation()
-    isOpen ? closeDropdown() : openDropdown()
+    void (isOpen ? closeDropdown() : openDropdown())
   })
   document.addEventListener("click", ev => {
     if (isOpen && !dropdown.contains(ev.target as Node)) closeDropdown()
   })
   window.addEventListener("scroll",
-    () => { if (isOpen) positionDropdown(); }, { passive: true })
+    () => { if (isOpen) positionDropdown() }, { passive: true })
   window.addEventListener("resize",
-    () => { if (isOpen) positionDropdown(); }, { passive: true })
+    () => { if (isOpen) positionDropdown() }, { passive: true })
 
   const recalculate = (): void => {
     if (list.clientWidth === 0) return
